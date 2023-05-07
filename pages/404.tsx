a2a951c4800dfaa273ba290597c258cbace437f9
ZAKHAROV-Artem <szakharovartem@gmail.com>
2023-05-07 13:49:45 +0200

    feat:not found page

---
+++ pages/404.tsx
@@ -0,0 +1,21 @@
+import Button from "@/components/inputs/Button";
+import { HOME } from "@/routes/routes";
+import { useRouter } from "next/router";
+
+export default function NotFound() {
+  const router = useRouter();
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <div className="text-6xl  text-white">404</div>
+      <div className="mt-3 px-3 text-center text-blue-400">
+        Hmm...this page doesn’t exist. Try searching for something else.
+      </div>
+      <Button
+        type="filled"
+        text="Search"
+        className="mt-3"
+        onClick={() => router.push(HOME)}
+      />
+    </div>
+  );
+}
