f021ae846770dd4b1137e9c2e302273a64df8041
ZAKHAROV-Artem <szakharovartem@gmail.com>
2023-05-16 20:03:13 +0200

    add: Current user avatar

---
+++ components/data-display/CurrentUserAvatar.tsx
@@ -0,0 +1,21 @@
+import Avatar from "@/components/data-display/Avatar";
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+interface CurrentUserAvatarProps {
+  size?: "sm" | "md" | "lg";
+  className?: string;
+}
+export default function CurrentUserAvatar({
+  size,
+  className,
+}: CurrentUserAvatarProps) {
+  const { data: user } = useCurrentUser();
+  return (
+    <Avatar
+      src={user?.profileImage || user?.image || ""}
+      name={user?.name || ""}
+      size={size}
+      className={className}
+    />
+  );
+}
