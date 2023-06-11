578905d890d82aacebe53532976ffd388e4e510b
ZAKHAROV-Artem <szakharovartem@gmail.com>
2023-06-11 22:47:44 +0200

    added title

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,15 +2,19 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 import SuggestBar from "./layout/SuggestBar";
 import AuthBar from "./layout/footer/AuthBar";
 import Sidebar from "./layout/sidebar/Sidebar";
+import Head from "next/head";
 
 interface Layout {
   children: JSX.Element;
 }
 export default function Layout({ children }: Layout) {
-  const { user, isSuccess, } = useCurrentUser();
- 
+  const { user, isSuccess } = useCurrentUser();
+
   return (
     <div className="h-screen bg-black">
+      <Head>
+        <title>Twitter</title>
+      </Head>
       <div className="xl:px-30 container mx-auto h-full max-w-6xl">
         <div className="relative grid h-full grid-cols-[80px_repeat(3,1fr)] md:grid-cols-[200px_repeat(3,1fr)]">
           <Sidebar />
